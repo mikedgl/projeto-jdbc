711d98b5fbe56c922c27531ba73425df5e59266c
Michael Douglas <mikedgls17@gmail.com>
2022-08-23 14:59:55 -0300

    Criação da classe Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,11 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
